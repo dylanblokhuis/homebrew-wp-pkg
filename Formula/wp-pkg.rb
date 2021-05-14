45a5936b6d9f672615b72729285f4238ff9fb916
Dylan Blokhuis <dylanblokhuis3@hotmail.com>
2021-05-14 15:05:12 +0200

    Update wp-pkg.rb

--- Formula/wp-pkg.rb
+++ Formula/wp-pkg.rb
@@ -1,9 +1,9 @@
 class WpPkg < Formula
   desc "Simple tool for packaging your WordPress project. Ignores non versioned files like node_modules and .git"
   homepage "https://github.com/dylanblokhuis/wp-pkg"
-  url "https://github.com/dylanblokhuis/wp-pkg/releases/download/v0.2.1/wp-pkg-x86_64-apple-darwin.tar.gz"
-  sha256 "14454bace5a65be2c82bb522b6fc754ca21f248a45fc917df9f64bc092f515bb"
-  version "0.2.1"
+  url "https://github.com/dylanblokhuis/wp-pkg/releases/download/v0.2.2/wp-pkg-x86_64-apple-darwin.tar.gz"
+  sha256 "03326b0f6029f73e38d912ed5422e49659a9de32d00ed229183b8a1ea7f1a234"
+  version "0.2.2"
 
   def install
     bin.install "wp-pkg"
